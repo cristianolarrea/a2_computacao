453f53270604463f74797afd98bd5332abc9424b
Cristiano Junior Larréa Leal <cristiano.leal@ntconsult.com.br>
2021-06-17 18:03:20 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,88 @@
+# Curso de Programação de Jogos com Pygame
+Criando um jogo de naves espaciais com Pygame. Para iniciantes em Python
+
+## Pré-requisitos
+Antes de começar este curso você precisa ter instalado em seu computador o git (com o qual irá clonar este repositório) e o pygame
+
+```bash
+pip install pygame
+```
+
+## Acompanhando o curso
+
+Para acompanhar o curso vamos usar `tags` do git para ir migrando o código através das várias etapas de desenvolvimento. Então antes de começar certifique-se de que todas as tags estão atualizadas em seu clone:
+
+```bash
+git fetch --all --tags
+```
+# O Jogo
+O jogo que iremos criar chama-se "Corona Shooter". e trata-se de um jogo de naves espaciais inspirado em uma pandemia de coronavirus.
+
+## Fase 0
+Na fase 0 começamos a organizar basicamente o nosso código. Para viajar no tempo até a faso 0 use o seguinte comando:
+
+```bash
+git checkout fase_0
+```
+Nesta fase criamos a estrutura básica do projeto e cria o módulo principal do jogo.
+
+O módulo principal se chama [main.py](/coronashooter/main.py) e fica no diretório coronashooter.
+
+Também criamos um arquivo de texto na raiz do nosso projeto, chamado de [requirements.txt](/requirements.txt) onde vamos listar todas as bibliotecas necessárias para o nosso jogo.
+
+Nesta fase, se executado, o módulo `main.py` apenas apresenta uma janela escura.
+
+## Fase 1
+Na fase 1 vamos usar uma classe para organizar a inicialização e os parâmetros do nosso jogo. Vamos também criar um módulo adicional para cuidar da especificação do plano de fundo do jogo.
+
+```bash
+git checkout fase_1_corrigida
+```
+
+Ao final desta fase temos nosso plano de fundo rolando de cima para baixo no nosso jogo. Para isso também adicionamos uma imagem de fundo a um diretório de imagens onde iremos manter todos os elementos gráficos do nosso projeto. 
+
+## Fase 2
+Nesta fase iremos adicionar outros controles ao jogo.
+```bash
+git checkout fase_2
+```
+Nesta fase movemos o tratamento de eventos para um método separado, e configuramos a tecla `ESC` como tecla de sair do jogo.
+
+## Fase 3
+Nesta fase vamos adicionar a Nave.
+
+```bash
+git checkout fase_3
+```
+Para isso, vamos criar mais [um módulo](/coronashooter/elementos.py) como uma classe de base para qualquer `sprite` do jogo.
+
+Em seguida iremos criar uma classe para a Nave do jogador e outra para as naves inimigas.
+
+Ao final desta fase poderemos ver as naves na tela.
+
+## Fase 4
+Nesta fase vamos adicionar movimento às naves. Neste processo vamos adicionar também algumas coisas a mais, como a geração de novos virus, controle movimentos da nave do jogador, outras coisas a mais. 
+
+Veja como o jogo ficou.
+```bash
+git checkout fase_4
+```
+
+## Fase 5
+Na fase 5 vamos adicionar tiros e colisões para completar o Jogo.
+
+Os tiros serão implementados como uma classe. Também foram implementados pontuação e passagem de fases pelo jogador.
+
+# Possibilidades de melhoria.
+Após estas cinco fases de desenvolvimento, temos um jogo funcional. Mas ainda existem muitas possibilidades de melhorias. Na lista abaixo temos algumas sugestões, mas use a sua criatividade e pense em novas maneiras de incrementar o jogo básico.
+
+- Adicional mostradores de pontos, nível e fase;
+- Trocar tela de fundo, quando passa de nível;  
+- Otimizar a [velocidade de atualização da tela](https://coderslegacy.com/improving-speed-performance-in-pygame/);
+- Adicionar outros elementos ao jogo, como Diferentes tipos de vacina e variantes do vírus, mais resistentes a um tipo de vacina.
+- Adicionar nível(is) chefão.
+- Adicionar trilha sonora e efeitos sonoros
+- Adicionar efeitos de explosão
+- etc.
+
+# Fim
---
+++ coronashooter/main.py
@@ -0,0 +1,322 @@
+import pygame
+from pygame.locals import (KEYDOWN,
+                           KEYUP,
+                           K_LEFT,
+                           K_RIGHT,
+                           QUIT,
+                           K_ESCAPE, K_UP, K_DOWN, K_RCTRL, K_LCTRL
+                           )
+from fundo import Fundo
+from elementos import ElementoSprite
+import random
+
+
+class Jogo:
+    def __init__(self, size=(1000, 1000), fullscreen=False):
+        self.elementos = {}
+        pygame.init()
+        flags = pygame.DOUBLEBUF
+        if fullscreen:
+            flags |= pygame.FULLSCREEN
+        self.tela = pygame.display.set_mode(size, flags=flags, depth=16)
+        self.fundo = Fundo()
+        self.jogador = None
+        self.interval = 0
+        self.nivel = 0
+        pygame.font.init()
+        self.fonte = pygame.font.SysFont('bitstreamverasans', 42)
+
+
+        self.screen_size = self.tela.get_size()
+        pygame.mouse.set_visible(0)
+        pygame.display.set_caption('Corona Shooter')
+        self.run = True
+
+
+    def escreve_placar(self):
+        vidas = self.fonte.render(f'vidas: {self.jogador.get_lives()*"❤"}', 1, (255, 255, 0), (0, 0, 0))
+        score = self.fonte.render(f'Score: {self.jogador.pontos}', 1, (255, 255, 0), (0, 0, 0))
+        self.tela.blit(vidas, (30, 30))
+        self.tela.blit(score, (self.screen_size[0] - 300, 30))
+
+    def manutenção(self):
+        r = random.randint(0, 100)
+        x = random.randint(1, self.screen_size[0])
+        virii = self.elementos["virii"]
+        if r > (10 * len(virii)):
+            enemy = Virus([0, 0])
+            size = enemy.get_size()
+            enemy.set_pos([min(max(x, size[0] / 2), self.screen_size[0] - size[0] / 2), size[1] / 2])
+            colisores = pygame.sprite.spritecollide(enemy, virii, False)
+            if colisores:
+                return
+            self.elementos["virii"].add(enemy)
+
+    def muda_nivel(self):
+        xp = self.jogador.get_pontos()
+        if xp > 10 and self.level == 0:
+            self.fundo = Fundo("tile2.png")
+            self.nivel = 1
+            self.jogador.set_lives(self.jogador.get_lives() + 3)
+        elif xp > 50 and self.level == 1:
+            self.fundo = Fundo("tile3.png")
+            self.nivel = 2
+            self.jogador.set_lives(self.player.get_lives() + 6)
+
+    def atualiza_elementos(self, dt):
+        self.fundo.update(dt)
+        for v in self.elementos.values():
+            v.update(dt)
+
+    def desenha_elementos(self):
+        self.fundo.draw(self.tela)
+        for v in self.elementos.values():
+            v.draw(self.tela)
+
+    def verifica_impactos(self, elemento, list, action):
+        """
+        Verifica ocorrência de colisões.
+        :param elemento: Instância de RenderPlain ou seja um grupo de sprites
+        :param list: lista ou grupo de sprites
+        :param action: função a ser chamada no evento de colisão
+        :return: lista de sprites envolvidos na colisão
+        """
+        if isinstance(elemento, pygame.sprite.RenderPlain):
+            hitted = pygame.sprite.groupcollide(elemento, list, 1, 0)
+            for v in hitted.values():
+                for o in v:
+                    action(o)
+            return hitted
+
+        elif isinstance(elemento, pygame.sprite.Sprite):
+            if pygame.sprite.spritecollide(elemento, list, 1):
+                action()
+            return elemento.morto
+
+    def ação_elemento(self):
+        """
+        Executa as ações dos elementos do jogo.
+        :return:
+        """
+        self.verifica_impactos(self.jogador, self.elementos["tiros_inimigo"],
+                               self.jogador.alvejado)
+        if self.jogador.morto:
+            self.run = False
+            return
+
+        # Verifica se o personagem trombou em algum inimigo
+        self.verifica_impactos(self.jogador, self.elementos["virii"],
+                               self.jogador.colisão)
+        if self.jogador.morto:
+            self.run = False
+            return
+        # Verifica se o personagem atingiu algum alvo.
+        hitted = self.verifica_impactos(self.elementos["tiros"],
+                                        self.elementos["virii"],
+                                        Virus.alvejado)
+
+        # Aumenta a pontos baseado no número de acertos:
+        self.jogador.set_pontos(self.jogador.get_pontos() + len(hitted))
+
+    def trata_eventos(self):
+        event = pygame.event.poll()
+        if event.type == pygame.QUIT:
+            self.run = False
+
+        if event.type in (KEYDOWN, KEYUP):
+            key = event.key
+            if key == K_ESCAPE:
+                self.run = False
+            elif key in (K_LCTRL, K_RCTRL):
+                self.interval = 0
+                self.jogador.atira(self.elementos["tiros"])
+            elif key == K_UP:
+                self.jogador.accel_top()
+            elif key == K_DOWN:
+                self.jogador.accel_bottom()
+            elif key == K_RIGHT:
+                self.jogador.accel_right()
+            elif key == K_LEFT:
+                self.jogador.accel_left()
+
+        keys = pygame.key.get_pressed()
+        if self.interval > 10:
+            self.interval = 0
+            if keys[K_RCTRL] or keys[K_LCTRL]:
+                self.jogador.atira(self.elementos["tiros"])
+
+    def loop(self):
+        clock = pygame.time.Clock()
+        dt = 16
+        self.elementos['virii'] = pygame.sprite.RenderPlain(Virus([120, 50]))
+        self.jogador = Jogador([200, 400], 5)
+        self.elementos['jogador'] = pygame.sprite.RenderPlain(self.jogador)
+        self.elementos['tiros'] = pygame.sprite.RenderPlain()
+        self.elementos['tiros_inimigo'] = pygame.sprite.RenderPlain()
+        while self.run:
+            clock.tick(1000 / dt)
+
+            self.trata_eventos()
+            self.ação_elemento()
+            self.manutenção()
+            # Atualiza Elementos
+            self.atualiza_elementos(dt)
+
+            # Desenhe no back buffer
+            self.desenha_elementos()
+            self.escreve_placar()
+            # texto = self.fonte.render(f"Vidas: {self.jogador.get_lives()}", True, (255, 255, 255), (0, 0, 0))
+
+            pygame.display.flip()
+
+
+class Nave(ElementoSprite):
+    def __init__(self, position, lives=0, speed=[0, 0], image=None, new_size=[83, 248]):
+        self.acceleration = [3, 3]
+        if not image:
+            image = "seringa.png"
+        super().__init__(image, position, speed, new_size)
+        self.set_lives(lives)
+
+    def get_lives(self):
+        return self.lives
+
+    def set_lives(self, lives):
+        self.lives = lives
+
+    def colisão(self):
+        if self.get_lives() <= 0:
+            self.kill()
+        else:
+            self.set_lives(self.get_lives() - 1)
+
+    def atira(self, lista_de_tiros, image=None):
+        s = list(self.get_speed())
+        s[1] *= 2
+        Tiro(self.get_pos(), s, image, lista_de_tiros)
+
+    def alvejado(self):
+        if self.get_lives() <= 0:
+            self.kill()
+        else:
+            self.set_lives(self.get_lives() - 1)
+
+    @property
+    def morto(self):
+        return self.get_lives() == 0
+
+    def accel_top(self):
+        speed = self.get_speed()
+        self.set_speed((speed[0], speed[1] - self.acceleration[1]))
+
+    def accel_bottom(self):
+        speed = self.get_speed()
+        self.set_speed((speed[0], speed[1] + self.acceleration[1]))
+
+    def accel_left(self):
+        speed = self.get_speed()
+        self.set_speed((speed[0] - self.acceleration[0], speed[1]))
+
+    def accel_right(self):
+        speed = self.get_speed()
+        self.set_speed((speed[0] + self.acceleration[0], speed[1]))
+
+
+class Virus(Nave):
+    def __init__(self, position, lives=1, speed=None, image=None, size=(100, 100)):
+        if not image:
+            image = "virus.png"
+        super().__init__(position, lives, speed, image, size)
+
+
+class Jogador(Nave):
+    """
+    A classe Player é uma classe derivada da classe GameObject.
+       No entanto, o personagem não morre quando passa da borda, este só
+    interrompe o seu movimento (vide update()).
+       E possui experiência, que o fará mudar de nivel e melhorar seu tiro.
+       A função get_pos() só foi redefinida para que os tiros não saissem da
+    parte da frente da nave do personagem, por esta estar virada ao contrário
+    das outras.
+    """
+
+    def __init__(self, position, lives=10, image=None, new_size=[83, 248]):
+        if not image:
+            image = "seringa.png"
+        super().__init__(position, lives, [0, 0], image, new_size)
+        self.pontos = 0
+
+    def update(self, dt):
+        move_speed = (self.speed[0] * dt / 16,
+                      self.speed[1] * dt / 16)
+        self.rect = self.rect.move(move_speed)
+
+        if (self.rect.right > self.area.right):
+            self.rect.right = self.area.right
+
+        elif (self.rect.left < 0):
+            self.rect.left = 0
+
+        if (self.rect.bottom > self.area.bottom):
+            self.rect.bottom = self.area.bottom
+
+        elif (self.rect.top < 0):
+            self.rect.top = 0
+
+    def get_pos(self):
+        return (self.rect.center[0], self.rect.top)
+
+    def get_pontos(self):
+        return self.pontos
+
+    def set_pontos(self, pontos):
+        self.pontos = pontos
+
+    def atira(self, lista_de_tiros, image=None):
+        l = 1
+        if self.pontos > 10:
+            l = 3
+        if self.pontos > 50:
+            l = 5
+
+        p = self.get_pos()
+        speeds = self.get_fire_speed(l)
+        for s in speeds:
+            Tiro(p, s, image, lista_de_tiros)
+
+    def get_fire_speed(self, shots):
+        speeds = []
+
+        if shots <= 0:
+            return speeds
+
+        if shots == 1:
+            speeds += [(0, -5)]
+
+        if shots > 1 and shots <= 3:
+            speeds += [(0, -5)]
+            speeds += [(-2, -3)]
+            speeds += [(2, -3)]
+
+        if shots > 3 and shots <= 5:
+            speeds += [(0, -5)]
+            speeds += [(-2, -3)]
+            speeds += [(2, -3)]
+            speeds += [(-4, -2)]
+            speeds += [(4, -2)]
+
+        return speeds
+
+
+class Tiro(ElementoSprite):
+    def __init__(self, position, speed=None, image=None, list=None):
+        if not image:
+            image = "tiro.png"
+        super().__init__(image, position, speed)
+        if list is not None:
+            self.add(list)
+
+
+if __name__ == '__main__':
+    J = Jogo(fullscreen=False)
+    J.loop()
